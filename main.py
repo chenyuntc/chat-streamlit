4ff2bf792e9c4a35b9323ae279d72fd6adbc23e4
chenyuntc <chenyuntc@163.com>
2023-03-02 21:20:27 -0500

    update

--- main.py
+++ main.py
@@ -1,50 +1,56 @@
+# code borrowed from https://medium.com/@avra42/build-your-own-chatbot-with-openai-gpt-3-and-streamlit-6f1330876846
 import openai
 import streamlit as st
 
-# pip install streamlit-chat
 from streamlit_chat import message
 
+openai.api_key = st.secrets["API_KEY"]
+history = []
 
-openai.api_key = st.secrets["API_KEY"] 
-def generate_response(prompt):
+def generate_response(prompt, history):
+    messagees = (
+        [
+            {"role": "system", "content": "You are a helpful assistant."},
+        ]
+        + history
+        + [
+            {"role": "user", "content": prompt},
+        ]
+    )
     completions = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=[
-                {"role": "system", "content": "You are a helpful assistant."},
-                {"role": "user", "content": prompt},
-                # {"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
-                # {"role": "user", "content": "Where was it played?"}
-            ]
-        )
+        model="gpt-3.5-turbo", messages=messagees
+    )
     message = completions.choices[0].message.content
     return message
 
-st.title("chatBot : Streamlit + openAI")
+
+st.title("YChat: a bot")
 
 # Storing the chat
-if 'generated' not in st.session_state:
-    st.session_state['generated'] = []
+if "generated" not in st.session_state:
+    st.session_state["generated"] = []
 
-if 'past' not in st.session_state:
-    st.session_state['past'] = []
+if "past" not in st.session_state:
+    st.session_state["past"] = []
 
 
-# We will get the user's input by calling the get_text function
 def get_text():
-    input_text = st.text_input("You: ","Hello, how are you?", key="input")
+    input_text = st.text_input("You: ", "Hello, how are you?", key="input")
     return input_text
+
 
 user_input = get_text()
 
 if user_input:
-    output = generate_response(user_input)
+    output = generate_response(user_input, history)
     # store the output
     st.session_state.past.append(user_input)
     st.session_state.generated.append(output)
+    history.append(dict(role="user", content=user_input))
+    history.append(dict(role="assistant", content=output))
+    history = history[:6]  # only keep last 3 conversations
 
-if st.session_state['generated']:
-
-    for i in range(len(st.session_state['generated'])-1, -1, -1):
+if st.session_state["generated"]:
+    for i in range(len(st.session_state["generated"]) - 1, -1, -1):
         message(st.session_state["generated"][i], key=str(i))
-        message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
-
+        message(st.session_state["past"][i], is_user=True, key=str(i) + "_user")
