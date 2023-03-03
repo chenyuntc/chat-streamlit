9fc04974e0efef3bbc8b76e962cde5d1556ec52f
chenyuntc <chenyuntc@163.com>
2023-03-02 21:30:17 -0500

    update

--- main.py
+++ main.py
@@ -5,7 +5,9 @@ import streamlit as st
 from streamlit_chat import message
 
 openai.api_key = st.secrets["API_KEY"]
-st.session_state['history'] = []
+if 'history' not in st.session_state:
+    st.session_state['history'] = []
+
 
 def generate_response(prompt, history):
     messagees = (
@@ -42,13 +44,13 @@ def get_text():
 user_input = get_text()
 
 if user_input:
-    output = generate_response(user_input, history)
+    output = generate_response(user_input, st.session_state['history'])
     # store the output
     st.session_state.past.append(user_input)
-    st.session_state.generated.append(output+str(history))
-    st.session_state['history'] .append(dict(role="user", content=user_input))
-    st.session_state['history'] .append(dict(role="assistant", content=output))
-    st.session_state['history']  = st.session_state['history'] [-6:]  # only keep last 3 conversations
+    st.session_state.generated.append(output)
+    st.session_state['history'].append(dict(role="user", content=user_input))
+    st.session_state['history'].append(dict(role="assistant", content=output))
+    st.session_state['history']  = st.session_state['history'][-6:]  # only keep last 3 conversations
 
 if st.session_state["generated"]:
     for i in range(len(st.session_state["generated"]) - 1, -1, -1):
