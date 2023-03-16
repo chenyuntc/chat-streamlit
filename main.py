73605b465eeef2051197364ca37b7fed20dc491c
chenyuntc <chenyuntc@163.com>
2023-03-16 14:33:33 -0400

    update + markdown

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 # code based on https://medium.com/@avra42/build-your-own-chatbot-with-openai-gpt-3-and-streamlit-6f1330876846
 import openai
 import streamlit as st
-
-from streamlit_chat import message
-
 openai.api_key = st.secrets["API_KEY"]
 if 'history' not in st.session_state:
     st.session_state['history'] = []
 
+def message_fn(messages, role='user'):
+    user_header = '**:blue[您: ]**'
+    bot_header = ':robot_face: '
+    messages = [f'''{bot_header} {answer} \n \n {user_header}  {question} \n \n
+      ''' for question,answer in zip (st.session_state['past'], st.session_state['generated'])][::-1]
+    markdown = '\n'.join(messages)
+    st.markdown(markdown,unsafe_allow_html=True)
 
 def generate_response(prompt, history):
     messagees = (
@@ -37,22 +41,20 @@ if "past" not in st.session_state:
 
 
 def get_text():
-    input_text = st.text_input("输入(回车发送): ", "", key="input",max_chars=512)
+    input_text = st.text_area("输入, ctrl+回车发送", "", key="input",max_chars=512,label_visibility='hidden')
+    st.button("发送")
     return input_text
 
 
 user_input = get_text()
-
 if user_input:
     output = generate_response(user_input, st.session_state['history'])
-    # store the output
     st.session_state.past.append(user_input)
     st.session_state.generated.append(output)
     st.session_state['history'].append(dict(role="user", content=user_input[:256]))
     st.session_state['history'].append(dict(role="assistant", content=output[:512]))
-    st.session_state['history']  = st.session_state['history'][-5:]  # only keep last 1.5 conversations
+    st.session_state['history']  = st.session_state['history'][-5:]  # only keep last 2.5 conversations
 
 if st.session_state["generated"]:
-    for i in range(len(st.session_state["generated"]) - 1, -1, -1):
-        message(st.session_state["generated"][i], key=str(i))
-        message(st.session_state["past"][i], is_user=True, key=str(i) + "_user")
+    message_fn('x')
+  
