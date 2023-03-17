4e75504cf54396a40a627d70d4306a957c455231
chenyuntc <chenyuntc@163.com>
2023-03-16 23:23:40 -0400

    stream openai

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import openai
 import streamlit as st
 
 openai.api_key = st.secrets["API_KEY"]
-
 hide_streamlit_style = """
             <style>
             #MainMenu {visibility: hidden;}
@@ -12,52 +11,48 @@ hide_streamlit_style = """
             """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
-
 def generate_response(prompt, history):
-    config = dict(role='system', content="You are a helpful assistant named YChat made by CloseAI")
+    config = dict(role='system', content="You are a helpful assistant named YChat made by CloseAI, be concise")
     prompt = dict(role='user',content=prompt)
     messages = [config, *history, prompt] 
-    completions = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo", messages=messages
-    )
-    message = completions.choices[0].message.content
-    return message
-
+    completions = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=messages,stream=True)
+    return completions
 
 # Storing the chat
 if "generated" not in st.session_state:
     st.session_state["generated"] = []
-
 if "past" not in st.session_state:
     st.session_state["past"] = []
 if "history" not in st.session_state:
     st.session_state["history"] = []
 
-
 def clear_text():
     user_input = st.session_state.get("input", None)
     if user_input:
-        output = generate_response(user_input, st.session_state["history"])
-        st.session_state.past.append(user_input)
-        st.session_state.generated.append(output)
-        st.session_state["history"].append(dict(role="user", content=user_input[:256]))
-        st.session_state["history"].append(dict(role="assistant", content=output[:512]))
-        st.session_state["history"] = st.session_state["history"][
-            -5:
-        ]  # only keep last 2.5 conversations
-        st.session_state["input"] = ""  # ,None)
         user_header = "**:blue[您: ]**"
         bot_header = ":robot_face:: "
+        output = generate_response(user_input, st.session_state["history"])
         messages = [
-            f""" {user_header}  {question} \n \n {bot_header} {answer}\n \n
-        """
+            f"{user_header}  {question} \n \n {bot_header} {answer}\n \n"
             for question, answer in zip(
                 st.session_state["past"], st.session_state["generated"]
             )
         ]
         markdown = "\n".join(messages)
-        st.markdown(markdown, unsafe_allow_html=True)
-
+        st.markdown(markdown)
+        st.markdown(f'{user_header} {user_input} \n\n')
+        res_st=st.empty()
+        res_text = ''
+        for xx in output:
+            if 'content'  in xx.choices[0]['delta']:
+                res_text += xx.choices[0]['delta']['content']
+                res_st.markdown(f"{bot_header} {res_text}")
+        st.session_state.past.append(user_input)
+        st.session_state.generated.append(res_text)
+        st.session_state["history"].append(dict(role="user", content=user_input[:256]))
+        st.session_state["history"].append(dict(role="assistant", content=res_text[-256:]))
+        st.session_state["history"] = st.session_state["history"][-4:]
+        st.session_state["input"] = ""  # ,None)
 
 st.text_input(
     "回车发送, 或点击空白位置", "", key="input", max_chars=512, on_change=clear_text
