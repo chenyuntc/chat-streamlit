bdb4c1893e1cb395bb9f0aa25705c088eb4379a2
chenyuntc <chenyuntc@163.com>
2023-03-17 21:41:50 -0400

    history

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 import openai
 import streamlit as st
+from google.cloud import firestore
+from datetime import datetime
+@st.cache_resource()
+def get_db():
+    firebase_setting=st.secrets['firebase']
+    db=firestore.Client.from_service_account_info(dict(project_id=firebase_setting['project_id'], private_key=firebase_setting['private_key'],token_uri=firebase_setting['token_uri'],client_email=firebase_setting['client_email']))
+    doc=db.collection('chat').document('history').collection((str(datetime.now())))
+    return doc
+db = get_db()
 
 openai.api_key = st.secrets["API_KEY"]
 hide_streamlit_style = """
@@ -8,7 +17,7 @@ hide_streamlit_style = """
             footer {visibility: hidden;}
             </style>
             """
-#st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
 def generate_response(prompt, history):
     config = dict(role='system', content="You are a helpful assistant named YChat made by CloseAI, be concise")
@@ -53,7 +62,7 @@ def clear_text():
         st.session_state["history"].append(dict(role="assistant", content=res_text[-256:]))
         st.session_state["history"] = st.session_state["history"][-4:]
         st.session_state["input"] = ""  # ,None)
+        db.document(str(datetime.now())).create(dict(response=res_text.replace('\n',r'\\n'),user_input=user_input))
 
 st.text_input(
-    "回车发送, 或点击空白位置", "", key="input", max_chars=512, on_change=clear_text
-)  # ,label_visibility='hidden')
+    "回车发送, 或点击空白位置", "", key="input", max_chars=512, on_change=clear_text,label_visibility='hidden' if len(st.session_state['history']) else 'visible')
